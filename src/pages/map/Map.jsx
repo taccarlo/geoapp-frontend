1e65f4eb257e9d83b917957b3c4c1ccde466eb78
Carlo Tacchella <tacchella.carlo@gmail.com>
2021-07-22 16:07:18 +0200

    [ADD] WMSTILELAYER

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -14,6 +14,8 @@ import {
   TileLayer,
   Marker,
   Popup,
+  LayersControl,
+  WMSTileLayer,
   MapConsumer,
 } from 'react-leaflet'
 import { dismissLocationModal } from '../../redux/actions'
@@ -60,6 +62,37 @@ export class Map extends Component {
               center={center}
               zoom={zoom}
             >
+      <LayersControl position="topright">
+      <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </LayersControl.BaseLayer>
+      <LayersControl.BaseLayer name="OpenStreetMap.BlackAndWhite">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+        />
+      </LayersControl.BaseLayer>
+
+      <LayersControl.Overlay name="experiment">
+
+      <WMSTileLayer
+      layers='OSM-Overlay-WMS'
+      url="http://ows.mundialis.de/services/service?"
+      transparent='true'
+      />
+
+      </LayersControl.Overlay>
+      <LayersControl.Overlay name="Marker with popup">
+        <Marker position={[45.439351, 10.99471]}>
+          <Popup>
+            Verona 2
+          </Popup>
+        </Marker>
+      </LayersControl.Overlay>
+    </LayersControl>
               <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -74,6 +107,7 @@ export class Map extends Component {
                 <Popup>Verona</Popup>
               </Marker>
               <LocationMarkers />
+              
             </MapContainer>
           )}
         </IonContent>
