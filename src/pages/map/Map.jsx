b37cb9ba64fab9054ced4922e27030ab95452bcd
xXAleXPolYXx <ale98sayan@gmail.com>
2021-07-27 15:40:23 +0200

    Aggiunta mappa in locale
    Aggiunte mappe in locale:
    -Circoscrizioni
    -Quartieri
    -Farmacie
    -Scuole
    -ZTL
    -Parcheggi Dedicati
    -Sosta Vietata

--- src/pages/map/Map.jsx
+++ src/pages/map/Map.jsx
@@ -22,7 +22,17 @@ import classes from './Map.module.css'
 
 import LocationMarkers from '../../components/location/LocationMarkers'
 
-import paesi from '../../data/countries.json'
+import axios from 'axios'
+
+import circoscrizioni from '../../data/circoscrizioni.json'
+import quartieri from '../../data/quartieri.json'
+import farmacie from '../../data/farmacie.json'
+import scuole from '../../data/scuole.json'
+import ztl from '../../data/ztl.json'
+import parcheggiDedicati from '../../data/parcheggiDedicati.json'
+import sostaVietata from '../../data/sostaVietata.json'
+
+//var urlCircoscrizioni= "http://192.168.20.20:8080/geoserver/circoscrizioni/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=circoscrizioni%3Acircoscrizioni&maxFeatures=50&outputFormat=application%2Fjson";
 
 export class Map extends Component {
   state = {
@@ -38,27 +48,49 @@ export class Map extends Component {
   }
 
   OnEachFarmacia = (farmacia, layer) =>{
-    layer.bindPopup(farmacia.properties.nome)
+    layer.bindPopup(farmacia.properties.denominazi)
   }
 
-  OnEachParco = (parco, layer) =>{
-    layer.bindPopup(parco.properties.nome)
+  OnEachScuola = (scuola, layer) =>{
+    layer.bindPopup(scuola.properties.nome_scuol)
   }
 
-  OnEachPaese = (paese, layer) =>{
-    layer.bindPopup(paese.properties.ADMIN)
+  OnEachZTL = (ztl, layer) =>{
+    layer.bindPopup(ztl.properties.nome)
+  }
+
+  OnEachParcheggioDedicato = (parcheggioDedicato, layer) =>{
+    layer.bindPopup(parcheggioDedicato.properties.nome)
+  }
+
+  OnEachSostaVietata = (sostaVietata, layer) =>{
+    layer.bindPopup(sostaVietata.properties.nome)
+  }
+
+  OnEachQuartiere = (quartiere, layer) =>{
+    layer.bindPopup(quartiere.properties.nome)
+  }
+
+  OnEachCircoscrizione = (paese, layer) =>{
+    layer.bindPopup(paese.properties.circoscriz)
 
     layer.on({
       click: (event) =>{
-        console.log("click on "+paese.properties.ADMIN)
+        console.log("click on "+paese.properties.circoscriz)
       }
     })
   }
 
   render() {
     const { center, zoom } = this.props.map
-    var farmacie = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia 5"},"geometry":{"type":"Point","coordinates":[10.832991600036621,45.43372583298752]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia 3"},"geometry":{"type":"Point","coordinates":[10.855522155761719,45.416286468478475]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia 4"},"geometry":{"type":"Point","coordinates":[10.783424377441406,45.42086519967432]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia1"},"geometry":{"type":"Point","coordinates":[10.99658489227295,45.44691472640307]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia 2"},"geometry":{"type":"Point","coordinates":[10.987358093261719,45.44730613046779]}}]};
-    var parchi = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"airport","nome":"Parco 1"},"geometry":{"type":"Point","coordinates":[11.034822463989258,45.4468846182856]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Parco 2"},"geometry":{"type":"Point","coordinates":[11.05748176574707,45.43357525704575]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Parco 3"},"geometry":{"type":"Point","coordinates":[11.715545654296875,45.31352900692258]}}]};
+    const headers = {
+      'crossdomain': 'true',
+      'Content-Type':'application/x-www-form-urlencoded',
+    };
+    //axios.get("http://192.168.20.20:8080/geoserver/circoscrizioni/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=circoscrizioni%3Acircoscrizioni&maxFeatures=50&outputFormat=application%2Fjson", { headers }).then(response => console.log(response));
+    //fetch('https://jsonplaceholder.typicode.com/todos/1') .then(response => response.json()) .then(data => console.log(data));
+
+    //var farmacie = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia 5"},"geometry":{"type":"Point","coordinates":[10.832991600036621,45.43372583298752]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia 3"},"geometry":{"type":"Point","coordinates":[10.855522155761719,45.416286468478475]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia 4"},"geometry":{"type":"Point","coordinates":[10.783424377441406,45.42086519967432]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia1"},"geometry":{"type":"Point","coordinates":[10.99658489227295,45.44691472640307]}},{"type":"Feature","properties":{"marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"","nome":"Farmacia 2"},"geometry":{"type":"Point","coordinates":[10.987358093261719,45.44730613046779]}}]};
     return (
       <IonPage>
         <IonHeader>
@@ -88,7 +120,7 @@ export class Map extends Component {
         />
       </LayersControl.BaseLayer>
 
-      <LayersControl.Overlay name="experiment">
+      <LayersControl.Overlay name="Confini">
 
       <WMSTileLayer
       layers='OSM-Overlay-WMS'
@@ -97,17 +129,36 @@ export class Map extends Component {
       />
 
       </LayersControl.Overlay>
-      <LayersControl.Overlay name="Marker with popup">
-        <Marker position={[45.439351, 10.99471]}>
-          <Popup>
-            Verona 2
-          </Popup>
-        </Marker>
+
+      <LayersControl.Overlay name="Circoscrizioni">
+      <GeoJSON key='circoscrizioni' data={circoscrizioni.features} onEachFeature={this.OnEachCircoscrizione} />
       </LayersControl.Overlay>
-    </LayersControl>
-              <GeoJSON key='farmacie' data={farmacie.features} onEachFeature={this.OnEachFarmacia} />
-              <GeoJSON key='parchi' data={parchi.features} onEachFeature={this.OnEachParco} />
-              <GeoJSON key='paesi' data={paesi.features} onEachFeature={this.OnEachPaese} />
+
+      <LayersControl.Overlay name="Quartieri">
+      <GeoJSON key='quartieri' data={quartieri.features} onEachFeature={this.OnEachQuartiere} />
+      </LayersControl.Overlay>
+
+      <LayersControl.Overlay name="Farmacie">
+      <GeoJSON key='farmacie' data={farmacie.features} onEachFeature={this.OnEachFarmacia} />
+      </LayersControl.Overlay>
+
+      <LayersControl.Overlay name="Scuole">
+      <GeoJSON key='scuole' data={scuole.features} onEachFeature={this.OnEachScuola} />
+      </LayersControl.Overlay>
+
+      <LayersControl.Overlay name="ZTL">
+      <GeoJSON key='ZTL' data={ztl.features} onEachFeature={this.OnEachZTL} />
+      </LayersControl.Overlay>
+
+      <LayersControl.Overlay name="Parcheggi Dedicati">
+      <GeoJSON key='parcheggiDedicati' data={parcheggiDedicati.features} onEachFeature={this.OnEachParcheggioDedicato} />
+      </LayersControl.Overlay>
+
+      <LayersControl.Overlay name="Sosta Vietata">
+      <GeoJSON key='sosta Vietata' data={sostaVietata.features} onEachFeature={this.OnEachSostaVietata} />
+      </LayersControl.Overlay>
+
+    </LayersControl>  
               <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
